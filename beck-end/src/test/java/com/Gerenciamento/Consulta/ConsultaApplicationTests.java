0b8b1b165fa0c35705431b7ad208bccea1d8f5dc
Patrik <patrik-israel@hotmail.com>
2024-11-03 18:19:03 -0300

    Sobrescrevendo o projeto, adicionando front para consumir a API

---
+++ beck-end/src/test/java/com/Gerenciamento/Consulta/ConsultaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Gerenciamento.Consulta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConsultaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
