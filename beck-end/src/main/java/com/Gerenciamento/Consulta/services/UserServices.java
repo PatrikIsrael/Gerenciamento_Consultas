2629990dfcab6ecffbd2d0bf2f5c48e5de9c94e4
Patrik <patrik-israel@hotmail.com>
2024-11-04 18:24:35 -0300

    UserService Adicionado

--- beck-end/src/main/java/com/Gerenciamento/Consulta/services/UserServices.java
+++ beck-end/src/main/java/com/Gerenciamento/Consulta/services/UserServices.java
@@ -1,4 +1,37 @@
 package com.Gerenciamento.Consulta.services;
 
+import com.Gerenciamento.Consulta.entity.User;
+import com.Gerenciamento.Consulta.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class UserServices {
+
+    @Autowired
+   private UserRepository userRepository;
+
+    public List<User>findAllUsers (){
+        return userRepository.findAll();
+    }
+
+    public User findById (Long Id){
+        return userRepository.findById(Id).orElse(null);
+    }
+
+    public User saveUser (User user){
+        return userRepository.save(user);
+    }
+
+    public User UpdateUser (Long Id, User user){
+        user.setId(Id);
+        return userRepository.save(user);
+    }
+
+    public void deleteUser(Long Id){
+        userRepository.deleteById(Id);
+    }
+
 }
