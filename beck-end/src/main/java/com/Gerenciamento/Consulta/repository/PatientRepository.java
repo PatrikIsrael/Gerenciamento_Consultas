c6ef19e6e42041674757aa29b9b91a5f1fc8c6c1
Patrik <patrik-israel@hotmail.com>
2024-11-06 09:19:20 -0300

    Novos metodos adicionados

--- beck-end/src/main/java/com/Gerenciamento/Consulta/repository/PatientRepository.java
+++ beck-end/src/main/java/com/Gerenciamento/Consulta/repository/PatientRepository.java
@@ -4,10 +4,16 @@ import com.Gerenciamento.Consulta.entity.Patient;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDate;
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface PatientRepository extends JpaRepository<Patient, Long> {
         List<Patient> findByName(String Name);
         List<Patient> findBycpf (String cpf);
+        boolean existsByCpf(String cpf);
+        Optional<Patient> findByCpf(String cpf);
+        List<Patient> findByNameContainingIgnoreCase(String name);
+        List<Patient> findByRegistrationDateBetween(LocalDate startDate, LocalDate endDate);
 }
