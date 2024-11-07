4056bc62d3c856475584e1cb7d350dcfd5781e23
Patrik <patrik-israel@hotmail.com>
2024-11-07 16:46:55 -0300

    Patient Controller implementado

--- beck-end/src/main/java/com/Gerenciamento/Consulta/controller/PatientController.java
+++ beck-end/src/main/java/com/Gerenciamento/Consulta/controller/PatientController.java
@@ -1,4 +1,60 @@
 package com.Gerenciamento.Consulta.controller;
 
+import com.Gerenciamento.Consulta.entity.Patient;
+import com.Gerenciamento.Consulta.services.PatientService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/patients")
 public class PatientController {
+
+    @Autowired
+    private PatientService patientService;
+
+    @GetMapping
+    public ResponseEntity<List<Patient>> getAllPatients() {
+        List<Patient> patients = patientService.findAllPatient();
+        return ResponseEntity.ok(patients);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Patient> getPatientById(@PathVariable Long id) {
+        Patient patient = patientService.findById(id);
+        if (patient != null) {
+            return ResponseEntity.ok(patient);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @PostMapping
+    public ResponseEntity<Patient> createPatient(@RequestBody Patient patient) throws IllegalAccessException {
+        Patient newPatient = patientService.savePatient(patient);
+        return ResponseEntity.status(HttpStatus.CREATED).body(newPatient);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Patient> updatePatient(@PathVariable Long id, @RequestBody Patient patientDetails) {
+        Patient updatedPatient = patientService.updatePatient(id, patientDetails);
+        if (updatedPatient != null) {
+            return ResponseEntity.ok(updatedPatient);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deletePatient(@PathVariable Long id) {
+        boolean isDeleted = patientService.deletePatient(id);
+        if (isDeleted) {
+            return ResponseEntity.noContent().build();
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
 }
