db833e29a5282512058f5fcfffb62bbdcabf3005
Patrik <patrik-israel@hotmail.com>
2024-11-07 18:14:11 -0300

    DoctorController Criado

--- beck-end/src/main/java/com/Gerenciamento/Consulta/controller/DoctorController.java
+++ beck-end/src/main/java/com/Gerenciamento/Consulta/controller/DoctorController.java
@@ -1,4 +1,66 @@
 package com.Gerenciamento.Consulta.controller;
 
+import com.Gerenciamento.Consulta.entity.Doctor;
+import com.Gerenciamento.Consulta.services.DoctorService;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/doctors")
 public class DoctorController {
+
+    @Autowired
+    private  DoctorService doctorService;
+
+
+    @GetMapping
+    public ResponseEntity<List<Doctor>> getAllDoctors() {
+        List<Doctor> doctors = doctorService.findAllDoctors();
+        return ResponseEntity.ok(doctors);
+    }
+
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Doctor> getDoctorById(@PathVariable Long id) {
+        Doctor doctor = doctorService.findById(id);
+        if (doctor != null) {
+            return ResponseEntity.ok(doctor);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+
+    @PostMapping
+    public ResponseEntity<Doctor> createDoctor(@RequestBody Doctor doctor) {
+        Doctor newDoctor = doctorService.saveDoctor(doctor);
+        return ResponseEntity.status(HttpStatus.CREATED).body(newDoctor);
+    }
+
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Doctor> updateDoctor(@PathVariable Long id, @RequestBody Doctor doctorDetails) {
+        Doctor updatedDoctor = doctorService.updateDoctor(id, doctorDetails);
+        if (updatedDoctor != null) {
+            return ResponseEntity.ok(updatedDoctor);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteDoctor(@PathVariable Long id) {
+        boolean isDeleted = doctorService.deleteDoctor(id);
+        if (isDeleted) {
+            return ResponseEntity.noContent().build();
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
 }
