ef3a6500c108931cc42ead2cb1495f3f88b14501
Patrik <patrik-israel@hotmail.com>
2024-11-07 18:12:53 -0300

    Ajuste da funcionalidade update  ConsultationService

--- beck-end/src/main/java/com/Gerenciamento/Consulta/services/ConsultationService.java
+++ beck-end/src/main/java/com/Gerenciamento/Consulta/services/ConsultationService.java
@@ -1,13 +1,11 @@
 package com.Gerenciamento.Consulta.services;
 
-
 import com.Gerenciamento.Consulta.entity.Consultation;
 import com.Gerenciamento.Consulta.repository.ConsultationRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
-import java.util.Optional;
 
 @Service
 public class ConsultationService {
@@ -15,23 +13,35 @@ public class ConsultationService {
     @Autowired
     private ConsultationRepository consultationRepository;
 
-    public List<Consultation> findAllAppointments() {
+    public List<Consultation> findAllConsultations() {
         return consultationRepository.findAll();
     }
 
-    public Optional<Consultation> findAppointmentById(Long id) {
-        return consultationRepository.findById(id);
+    public Consultation findConsultationById(Long id) {
+        return consultationRepository.findById(id).orElse(null);
     }
 
-    public Consultation saveAppointment(Consultation consultation) {
+    public Consultation saveConsultation(Consultation consultation) {
         return consultationRepository.save(consultation);
     }
 
-    public void deleteAppointment(Long id) {
+    public Consultation updateConsultation(Long id, Consultation consultationDetails) {
+        Consultation existingConsultation = consultationRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Consulta não encontrada"));
+
+        existingConsultation.setConsultationDate(consultationDetails.getConsultationDate());
+        existingConsultation.setStatus(consultationDetails.getStatus());
+        existingConsultation.setDoctor(consultationDetails.getDoctor());
+        existingConsultation.setPatient(consultationDetails.getPatient());
+
+        return consultationRepository.save(existingConsultation);
+    }
+
+    public void deleteConsultation(Long id) {
         consultationRepository.deleteById(id);
     }
 
-    public List<Consultation> findAppointmentsByStatus(String status) {
+    public List<Consultation> findConsultationsByStatus(String status) {
         return consultationRepository.findByStatus(status);
     }
 }
