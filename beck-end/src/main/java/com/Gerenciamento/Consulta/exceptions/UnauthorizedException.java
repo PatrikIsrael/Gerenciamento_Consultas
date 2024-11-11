78f3ab5cef0c2f91112aca038dfe845e85c352cb
Patrik <patrik-israel@hotmail.com>
2024-11-11 12:59:01 -0300

    UnauthorizedException criada

--- beck-end/src/main/java/com/Gerenciamento/Consulta/exceptions/UnauthorizedException.java
+++ beck-end/src/main/java/com/Gerenciamento/Consulta/exceptions/UnauthorizedException.java
@@ -1,4 +1,8 @@
 package com.Gerenciamento.Consulta.exceptions;
 
-public class UnauthorizedException {
+public class UnauthorizedException extends RuntimeException {
+    public UnauthorizedException(String message) {
+        super(message);
+    }
 }
+
