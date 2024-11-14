d0089a0e9965630897c829d39e80672c9d097815
Patrik <patrik-israel@hotmail.com>
2024-11-13 22:13:34 -0300

    method login/register all permission,  admin***

--- beck-end/src/main/java/com/Gerenciamento/Consulta/infra/SecurityConfigurations.java
+++ beck-end/src/main/java/com/Gerenciamento/Consulta/infra/SecurityConfigurations.java
@@ -2,6 +2,7 @@ package com.Gerenciamento.Consulta.infra;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -15,7 +16,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 
 
 @Configuration
-@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityConfigurations {
 
     @Bean
@@ -24,8 +25,11 @@ public class SecurityConfigurations {
                 .csrf(csrf -> csrf.disable())
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(authorize -> authorize
-                        .requestMatchers("/**").permitAll()
-                )
+                .requestMatchers(HttpMethod.POST, "/login").permitAll()
+                .requestMatchers(HttpMethod.POST, "/register").permitAll()
+                .requestMatchers("/admin/**").hasRole("ADMIN")
+                .anyRequest().authenticated()
+        )
                 .build();
     }
 
