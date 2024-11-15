95e6bc40a879cdc6c4c7f962e36b09db82d5f7a8
Patrik <patrik-israel@hotmail.com>
2024-11-15 12:32:31 -0300

    finbylogin adicionado

--- beck-end/src/main/java/com/Gerenciamento/Consulta/repository/UserRepository.java
+++ beck-end/src/main/java/com/Gerenciamento/Consulta/repository/UserRepository.java
@@ -5,7 +5,10 @@ import com.Gerenciamento.Consulta.entity.UserRole;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
+
 
 public interface UserRepository extends JpaRepository<User, Long> {
     List<User> findByRole(UserRole role);
+    Optional<User> findByLogin(String login);
 }
