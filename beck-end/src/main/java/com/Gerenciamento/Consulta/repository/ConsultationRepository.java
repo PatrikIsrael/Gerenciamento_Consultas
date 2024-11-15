ef93452da8ab67516dc318caba7ae71cbffb0561
Patrik <patrik-israel@hotmail.com>
2024-11-15 12:35:00 -0300

    new implements

--- beck-end/src/main/java/com/Gerenciamento/Consulta/repository/ConsultationRepository.java
+++ beck-end/src/main/java/com/Gerenciamento/Consulta/repository/ConsultationRepository.java
@@ -1,14 +1,20 @@
 package com.Gerenciamento.Consulta.repository;
 
 import com.Gerenciamento.Consulta.entity.Consultation;
+import com.Gerenciamento.Consulta.entity.Doctor;
+import com.Gerenciamento.Consulta.entity.Patient;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+
 @Repository
-public interface ConsultationRepository extends JpaRepository <Consultation, Long>{
-    List<Consultation> findByPatientId(Long patientId);
-    List<Consultation> findByDoctorId (Long DoctorId);
+public interface ConsultationRepository extends JpaRepository<Consultation, Long> {
+    List<Consultation> findByPatient(Patient patient);
+    List<Consultation> findByPatient_Id(Long patientId);
+    List<Consultation> findByDoctor(Doctor doctor);
     List<Consultation> findByStatus(String status);
 
 }
+
+
