d2081a0d0cd6960443f46ba6c529d41465edaa02
Patrik <patrik-israel@hotmail.com>
2024-11-21 21:44:39 -0300

    added patientMapper

--- beck-end/src/main/java/com/Gerenciamento/Consulta/mapper/PatientMapper.java
+++ beck-end/src/main/java/com/Gerenciamento/Consulta/mapper/PatientMapper.java
@@ -1,4 +1,14 @@
 package com.Gerenciamento.Consulta.mapper;
 
-public class PatientMapper {
+import com.Gerenciamento.Consulta.dto.PatientDTO;
+import com.Gerenciamento.Consulta.entity.Patient;
+
+import java.util.List;
+
+public interface PatientMapper {
+    Patient toEntity(PatientDTO patientDTO);
+    PatientDTO toDTO(Patient patient);
+
+    List<PatientDTO> toDTOList(List<Patient> patients);
 }
+
