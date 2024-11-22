f398da95b976e7af096a73641711cd5149e73d4e
Patrik <patrik-israel@hotmail.com>
2024-11-21 21:41:24 -0300

    adjustments to be the same DTO

--- beck-end/src/main/java/com/Gerenciamento/Consulta/entity/Patient.java
+++ beck-end/src/main/java/com/Gerenciamento/Consulta/entity/Patient.java
@@ -7,6 +7,7 @@ import org.hibernate.annotations.UpdateTimestamp;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.util.HashSet;
 
 @Entity
 @Table(name = "patients")
@@ -48,4 +49,5 @@ public class Patient {
 
     @UpdateTimestamp
     private LocalDateTime updatedAt;
+
 }
