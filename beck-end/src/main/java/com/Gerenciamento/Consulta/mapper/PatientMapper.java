fa62b29cd8255d4eac879f08a7e2872099ac64fb
Patrik <patrik-israel@hotmail.com>
2024-11-21 22:09:19 -0300

    adjustment in import

--- beck-end/src/main/java/com/Gerenciamento/Consulta/mapper/PatientMapper.java
+++ beck-end/src/main/java/com/Gerenciamento/Consulta/mapper/PatientMapper.java
@@ -1,14 +1,15 @@
 package com.Gerenciamento.Consulta.mapper;
 
 import com.Gerenciamento.Consulta.dto.PatientDTO;
-import com.Gerenciamento.Consulta.entity.Patient;
+import com.Gerenciamento.Consulta.entities.Patient;
+import org.mapstruct.Mapper;
 
 import java.util.List;
 
+@Mapper(componentModel = "spring")
 public interface PatientMapper {
     Patient toEntity(PatientDTO patientDTO);
     PatientDTO toDTO(Patient patient);
-
     List<PatientDTO> toDTOList(List<Patient> patients);
 }
 
