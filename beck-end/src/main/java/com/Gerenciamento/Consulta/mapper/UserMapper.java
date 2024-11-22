6e0bc9f34cb3e3b9fe6ad86d6766923186ecb8c9
Patrik <patrik-israel@hotmail.com>
2024-11-21 22:46:43 -0300

    Created UserMapper

--- beck-end/src/main/java/com/Gerenciamento/Consulta/mapper/UserMapper.java
+++ beck-end/src/main/java/com/Gerenciamento/Consulta/mapper/UserMapper.java
@@ -1,4 +1,16 @@
 package com.Gerenciamento.Consulta.mapper;
 
-public class UserMapper {
+import com.Gerenciamento.Consulta.dto.UserDTO;
+import com.Gerenciamento.Consulta.entities.User;
+import org.mapstruct.Mapper;
+
+import java.util.List;
+
+@Mapper(componentModel = "spring")
+public interface UserMapper {
+    User toEntity(UserDTO userDTO);
+    UserDTO toDTO(User user);
+
+    List<UserDTO> toDTOList(List<User> users);
+    List<User> toEntityList(List<UserDTO> userDTOs);
 }
