0ab138ac8714c29e2d220353defd72723bdc3df5
Patrik <patrik-israel@hotmail.com>
2024-11-21 21:45:58 -0300

    adjust PatientService to use PatientMapper and improve organization

--- beck-end/src/main/java/com/Gerenciamento/Consulta/service/PatientService.java
+++ beck-end/src/main/java/com/Gerenciamento/Consulta/service/PatientService.java
@@ -1,9 +1,11 @@
 package com.Gerenciamento.Consulta.service;
 
+import com.Gerenciamento.Consulta.dto.PatientDTO;
 import com.Gerenciamento.Consulta.entity.Patient;
 import com.Gerenciamento.Consulta.exceptions.InvalidRequestException;
 import com.Gerenciamento.Consulta.exceptions.ResourceNotFoundException;
 import com.Gerenciamento.Consulta.repository.PatientRepository;
+import com.Gerenciamento.Consulta.mapper.PatientMapper;  // Importando o mapper
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -13,41 +15,57 @@ import java.util.List;
 @Service
 public class PatientService {
 
+    private final PatientRepository patientRepository;
+    private final PatientMapper patientMapper;
+
     @Autowired
-    private PatientRepository patientRepository;
-
-    public List<Patient> findAllPatients() {
-        return patientRepository.findAll();
+    public PatientService(PatientRepository patientRepository, PatientMapper patientMapper) {
+        this.patientRepository = patientRepository;
+        this.patientMapper = patientMapper;
     }
 
-    public Patient findPatientById(Long id) {
-        return patientRepository.findById(id)
+    public List<PatientDTO> findAllPatients() {
+        List<Patient> patients = patientRepository.findAll();
+        return patientMapper.toDTOList(patients);
+    }
+
+    public PatientDTO findPatientById(Long id) {
+        Patient patient = patientRepository.findById(id)
                 .orElseThrow(() -> new ResourceNotFoundException("Paciente com o ID " + id + " não encontrado."));
+        return patientMapper.toDTO(patient);
     }
 
-    public Patient savePatient(Patient patient) {
+    public PatientDTO savePatient(PatientDTO patientDTO) {
+
+        Patient patient = patientMapper.toEntity(patientDTO);
+
         if (patientRepository.existsByCpf(patient.getCpf())) {
             throw new InvalidRequestException("O CPF " + patient.getCpf() + " já está cadastrado.");
         }
-        return patientRepository.save(patient);
+
+        Patient savedPatient = patientRepository.save(patient);
+        return patientMapper.toDTO(savedPatient);
     }
 
-    public Patient updatePatient(Long id, Patient patientDetails) {
+    public PatientDTO updatePatient(Long id, PatientDTO patientDTO) {
         Patient existingPatient = patientRepository.findById(id)
                 .orElseThrow(() -> new ResourceNotFoundException("Paciente com o ID " + id + " não encontrado."));
 
-        if (!existingPatient.getCpf().equals(patientDetails.getCpf()) &&
-                patientRepository.existsByCpf(patientDetails.getCpf())) {
-            throw new InvalidRequestException("O CPF " + patientDetails.getCpf() + " já está cadastrado.");
+        if (!existingPatient.getCpf().equals(patientDTO.getCpf()) &&
+                patientRepository.existsByCpf(patientDTO.getCpf())) {
+            throw new InvalidRequestException("O CPF " + patientDTO.getCpf() + " já está cadastrado.");
         }
 
-        existingPatient.setName(patientDetails.getName());
-        existingPatient.setCpf(patientDetails.getCpf());
-        existingPatient.setDateOfBirth(patientDetails.getDateOfBirth());
-        existingPatient.setPhoneNumber(patientDetails.getPhoneNumber());
-        existingPatient.setMedicalHistory(patientDetails.getMedicalHistory());
 
-        return patientRepository.save(existingPatient);
+        existingPatient.setName(patientDTO.getName());
+        existingPatient.setCpf(patientDTO.getCpf());
+        existingPatient.setDateOfBirth(LocalDate.parse(patientDTO.getDateOfBirth()));
+        existingPatient.setPhoneNumber(patientDTO.getPhoneNumber());
+        existingPatient.setMedicalHistory(patientDTO.getMedicalHistory());
+
+
+        Patient updatedPatient = patientRepository.save(existingPatient);
+        return patientMapper.toDTO(updatedPatient);
     }
 
     public boolean deletePatient(Long id) {
@@ -58,20 +76,23 @@ public class PatientService {
         return false;
     }
 
-    public List<Patient> findByName(String name) {
-        return patientRepository.findByNameContainingIgnoreCase(name);
+    public List<PatientDTO> findByName(String name) {
+        List<Patient> patients = patientRepository.findByNameContainingIgnoreCase(name);
+        return patientMapper.toDTOList(patients);
     }
 
-    public Patient findByCpf(String cpf) {
-        return patientRepository.findByCpf(cpf)
+    public PatientDTO findByCpf(String cpf) {
+        Patient patient = patientRepository.findByCpf(cpf)
                 .orElseThrow(() -> new ResourceNotFoundException("Paciente com o CPF " + cpf + " não encontrado."));
+        return patientMapper.toDTO(patient);
     }
 
     public long countAllPatients() {
         return patientRepository.count();
     }
 
-    public List<Patient> findByRegistrationDate(LocalDate startDate, LocalDate endDate) {
-        return patientRepository.findByRegistrationDateBetween(startDate, endDate);
+    public List<PatientDTO> findByRegistrationDate(LocalDate startDate, LocalDate endDate) {
+        List<Patient> patients = patientRepository.findByRegistrationDateBetween(startDate, endDate);
+        return patientMapper.toDTOList(patients);
     }
 }
