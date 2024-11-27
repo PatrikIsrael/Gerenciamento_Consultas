fc5022024f556386aee9e73a613498b76ed94135
Patrik <patrik-israel@hotmail.com>
2024-11-27 09:17:37 -0300

    ConsultationMapper added

--- beck-end/src/main/java/com/Gerenciamento/Consulta/mapper/ConsultationMapper.java
+++ beck-end/src/main/java/com/Gerenciamento/Consulta/mapper/ConsultationMapper.java
@@ -1,4 +1,15 @@
 package com.Gerenciamento.Consulta.mapper;
 
+import com.Gerenciamento.Consulta.dto.ConsultationDTO;
+import com.Gerenciamento.Consulta.entities.Consultation;
+import org.mapstruct.Mapper;
+
+import java.util.List;
+
+@Mapper(componentModel = "spring" )
 public interface ConsultationMapper {
+    Consultation toEntity (ConsultationDTO consultationDTO);
+    ConsultationDTO toDTO (Consultation consultation);
+    List<ConsultationDTO> toDTOList (List<Consultation> consultation);
+
 }
