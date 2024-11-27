1b0eb1d280e197e5f79eb182bf904d1e1a819fec
Patrik <patrik-israel@hotmail.com>
2024-11-27 09:17:07 -0300

    DoctorMapper added

--- beck-end/src/main/java/com/Gerenciamento/Consulta/mapper/DoctorMapper.java
+++ beck-end/src/main/java/com/Gerenciamento/Consulta/mapper/DoctorMapper.java
@@ -1,4 +1,14 @@
 package com.Gerenciamento.Consulta.mapper;
 
+import com.Gerenciamento.Consulta.dto.DoctorDTO;
+import com.Gerenciamento.Consulta.entities.Doctor;
+import org.mapstruct.Mapper;
+
+import java.util.List;
+
+@Mapper(componentModel = "spring")
 public interface DoctorMapper {
+    Doctor toEntity(DoctorDTO doctorDTO);
+    DoctorDTO toDTO(Doctor doctor);
+    List<DoctorDTO> toDTOList(List<Doctor> doctor);
 }
